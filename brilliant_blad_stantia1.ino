df3077b39c5911f18a2ecdafdcdcc3061e82828f
Wanjikujanee <99467506+Wanjikujanee@users.noreply.github.com>
2022-03-11 19:29:01 +0300

    Add files via upload

---
+++ brilliant_blad_stantia1.ino
@@ -0,0 +1,28 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(1,OUTPUT);
+          
+  pinMode(2,OUTPUT);
+  
+  pinMode(3,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(2,HIGH);// Wait for 1000 millisecond(s)
+  delay(1000);
+  digitalWrite(2,LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  
+  digitalWrite(3,HIGH);
+  delay(1000); // Wait for 1000 milliseconds(s)
+  digitalWrite(3,LOW);
+  delay(1000); // Wait for 1000 milliseconds(s)
+}
