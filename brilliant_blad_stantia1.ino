7123ec23c9e4dac23bab181a9551a921195a6597
hilda1737 <hildamoraahh@gmail.com>
2022-03-29 09:38:17 +0300

    changes

--- brilliant_blad_stantia1.ino
+++ brilliant_blad_stantia1.ino
@@ -11,7 +11,7 @@ void setup()
 
 void loop()
 {
-  digitalWrite(1, HIGH);
+  digitalWrite(1, );
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(1, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
